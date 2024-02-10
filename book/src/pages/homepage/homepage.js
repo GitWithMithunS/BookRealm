064329e760d2416cf6cfc341cb8ce9edd99d841a
--get <chakravarthimithun62@gmail.com>
2024-02-10 20:56:56 +0530

    book details_page_ui

--- book/src/pages/homepage/homepage.js
+++ book/src/pages/homepage/homepage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Showcase from "../../components/layouts/showcase/Showcase.js";
 import ProductListing from "../../components/layouts/product-listing/ProductListing.js";
-// import Footer from '../../components/layouts/footer/Footer';
+
+
+
+
 
 const HomePage = () => {
     return (
         <section>
             <Showcase />
-             <ProductListing />
-            {/*<Footer /> */}
+            <ProductListing />
+
         </section>
     )
 }
